ecdcbe775a949d4d9a544507930c623d04a364de
koteitan <ukaibutton@mac.com>
2025-12-07 21:11:35 +0900

    bump up to version 0.2

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import type { TimelineEvent } from './nostr/types';
 import './App.css';
 
 // Version: Update this on each deployment
-const APP_VERSION = '0.1.34';
+const APP_VERSION = '0.2.0';
 const APP_NAME = '(.>_<)-(.>_<)-mojimoji';
 
 // Format build timestamp based on locale
