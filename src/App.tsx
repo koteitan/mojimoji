b7b003585cb524c60a640862ade70f26bc90df66
koteitan <ukaibutton@mac.com>
2026-01-01 11:43:47 +0900

    bump v1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import type { TimelineItem } from './nostr/types';
 import './App.css';
 
 // Version: Update this on each deployment
-export const APP_VERSION = '0.13.1';
+export const APP_VERSION = '1.0.0';
 
 const APP_NAME = '(.>_<)-(.>_<)-mojimoji: Nostr Modular Client';
 const LOADING_PREFIX = '(.>_<)-(.>_<)-loading ';
